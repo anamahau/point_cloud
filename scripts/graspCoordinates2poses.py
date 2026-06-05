a0119993b5268d5fd89daad7ac90af084b9220c9
Anamarija <anamarija.hauptman@gmail.com>
2026-06-05 07:01:55 +0000

    fixed coordinates2poses script

--- scripts/graspCoordinates2poses.py
+++ scripts/graspCoordinates2poses.py
@@ -7,6 +7,7 @@ import subprocess
 import numpy as np
 from PIL import Image
 from tf_reader import getTfTransform
+from scipy.spatial.transform import Rotation as R
 
 
 def run_cmd(cmd):
@@ -39,7 +40,27 @@ def read_goal_coordinates():
     fy = f["fy"]
     cx = c["cx"]
     cy = c["cy"]
-    transformMatrix = getTfTransform('base_link', 'rgbd_depth_optical_frame')
+    file = f'{FOLDER_PATH}/{FOLDER_NAME}/observation_start/camera_pose_in_world.json'
+    with open(file, 'r') as f:
+        data = json.load(f)
+    roll = data["rotation_euler_xyz_in_radians"]["roll"]
+    pitch = data["rotation_euler_xyz_in_radians"]["pitch"]
+    yaw = data["rotation_euler_xyz_in_radians"]["yaw"]
+    rotation = R.from_euler('xyz', [roll, pitch, yaw])
+    rotation_matrix = rotation.as_matrix()
+    transformMatrix_base2orb = np.eye(4)
+    transformMatrix_base2orb[:3, :3] = rotation_matrix
+    transformMatrix_base2orb[:3, 3] = [
+        data["position_in_meters"]["x"],
+        data["position_in_meters"]["y"],
+        data["position_in_meters"]["z"]
+    ]
+    transformMatrix_orb2rs = np.array([
+            [ 0.9989575 , 0.04022338,  0.02158668, -0.00320142],
+            [-0.03988856, 0.99908041, -0.0157236 , -0.10964579],
+            [-0.02219929, 0.01484615,  0.99964333, -0.05616454],
+            [ 0.0       , 0.0       ,  0.0       ,  1.0       ]
+        ])
     file = f'{FOLDER_PATH}/{FOLDER_NAME}/grasp_predicted/grasp_coordinates.json'
     with open(file, 'r') as f:
         data = json.load(f)
@@ -50,7 +71,8 @@ def read_goal_coordinates():
         z = depth_img[v, u]
         x = (u - cx) * z / fx
         y = (v - cy) * z / fy
-        xyz = transformMatrix @ np.array([x, y, z, 1])
+        xyz_tmp = transformMatrix_orb2rs @ np.array([x, y, z, 1])
+        xyz = transformMatrix_base2orb @ xyz_tmp
         if (z == 0.0):
             print('point', i, '-', xyz[:3], '- invalid depth')
             cv2.circle(rgb_img, (u, v), 5, (0, 0, 255), -1)
@@ -73,7 +95,7 @@ REMOTE_USER = 'anamarija'
 REMOTE_HOST = '178.172.42.49'
 REMOTE_PATH = '/home/anamarija/cedirnet-dev/tools/unfolding_evaluation_ral2025/grasp_inference'
 FOLDER_PATH = '/talos_ws/dataForCedirnet'
-FOLDER_NAME = 'sample_000021_2'
+FOLDER_NAME = 'sample_000027'
 
 
 if __name__ == '__main__':
